f7342a65ce8394af288d8643da9b2729b29fa80e
John Vrbanac <john.vrbanac@linux.com>
2014-09-20 19:24:44 -0500

    Setting up tox and adding initial setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+requirements = ['six']
+
+desc = ''
+with open('README.rst') as f:
+    desc = f.read()
+
+setup(
+    name='alchemize',
+    version='0.0.1',
+    description=('A simple library that allows for serialization and '
+                 'deserialization of models via mapping definitions'),
+    long_description=(desc),
+    url='https://github.com/jmvrbanac/alchemize',
+    author='John Vrbanac',
+    author_email='john.vrbanac@linux.com',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: POSIX :: Linux'
+    ],
+    keywords='model serialize deserialize transmute',
+    packages=find_packages(exclude=['contrib', 'docs', 'spec*']),
+    install_requires=requirements,
+    package_data={},
+    data_files=[],
+    entry_points={}
+)
