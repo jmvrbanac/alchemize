4d9d4fcd1aa34678603d751458f8346b7aedf7af
John Vrbanac <john.vrbanac@linux.com>
2019-03-25 01:07:55 -0500

    Bump version: 0.13.2 → 0.14.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='alchemize',
-    version='0.13.2',
+    version='0.14.0',
     description=('A simple library that allows for serialization and '
                  'deserialization of models via mapping definitions'),
     long_description=(desc),
